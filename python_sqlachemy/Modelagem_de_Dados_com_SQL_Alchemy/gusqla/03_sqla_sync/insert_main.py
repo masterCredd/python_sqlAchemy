d1752912226e64d6519ea51102c141fee9baea9b
Edgar Soares <ed.soares@hotmail.com>
2022-11-25 13:13:24 -0300

    insert daddos

---
+++ python_sqlachemy/Modelagem_de_Dados_com_SQL_Alchemy/gusqla/03_sqla_sync/insert_main.py
@@ -0,0 +1,33 @@
+from venv import create
+from sqlalchemy import insert
+from conf.db_session import craete_session
+from model.aditivo_nutritivo import AditivoNutritivo
+
+def insert_aditivo_nutritivo():
+    """
+    The insert_aditivo_nutritivo function inserts a new Aditivo Nutritivo into the database.
+    It requires no input and returns nothing.
+
+    :return: :
+    :doc-author: Trelent
+    """
+    print('Cadastrando Aditivo Nutritivo')
+
+    nome= input('Informe o nome do Aditivo Nutritivo:')
+
+    formula_quimita=input('Informe a fórmula química do Aditivo Nutritivo:')
+
+    an =AditivoNutritivo(nome=nome,formula_quimita=formula_quimita)
+
+    with craete_session() as session:
+        try:
+            session.add(an)
+            session.commit()
+        except:
+            print('Erro de inserção de dados')
+
+    print('Aditivo Nutritivo cadastrado com sucesso!')
+    print(f'ID: {an.id}')
+    print(f'Data de Criação: {an.data_criacao}')
+    print(f'Nome: {an.nome}')
+    print(f'Formula Quimica: {an.formula_quimica}')
